298bd291bcd52270ffb4d646c52972df24c36b80
NeunMonde <timon.wellhausen@outlook.com>
2024-11-19 22:36:31 +0100

    Aufgabe 1

--- Assignment05/primes.c
+++ Assignment05/primes.c
@@ -4,35 +4,37 @@ Run: ./primes
 make primes && ./primes
 */
 
-#include "Bas.eh" //include prog1lib  
+#include "Base.h" //include prog1lib // FIX: Typo "Bas.eh" to "Base.h"  
 /*
     Prints the primes in the given interval [lower, upper)].
 */
 int print_primes_in_intervall(int lower, int upper){
     
-    if( upper > lower){
+    if (upper > lower){ 
         int prime = lower;
-        if(prime < 2) 
-            prime = 2;;
+        if(prime < 2){ // FIX: Missing bracket
+            prime = 2; // FIX: Too many semicolons (no effect on code)
         }
         int count = 0;
         while (prime < upper){  
-            int i = 2
+            int i = 2; // FIX: Missing semicolon
             bool is_a_prime = true;
             while(i <= (prime >> 1)){
-                    if( prime % i = 0 ){
+                if(prime % i == 0){ // FIX: = used instead of ==, indent
                     is_a_prime = false;
                     break;
                 }
                 i++;
             }
-            if(is_a_prime)
+            if(is_a_prime) { // FIX: Missing brackets
                 count++;
                 printf("Die %2d. Primzahl im Intervall: [%d,%d)] ist: %2d\n", count, lower, upper, prime);
+            } // FIX: Missing brackets
             prime++;
         }
         return count;
     }
+    return 0; // FIX: Return value missing
 }
 
 void test_print_primes_in_intervall(){
