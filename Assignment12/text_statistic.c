0a7cf86ce73c0109fe1af625f0220abff68dbca9
NeunMonde <timon.wellhausen@outlook.com>
2025-01-23 17:54:07 +0100

    Aufgabe 1 fertig

--- Assignment12/text_statistic.c
+++ Assignment12/text_statistic.c
@@ -8,20 +8,67 @@ typedef struct text_statistic_s {
     int sentences;
 } TextStatistic;
 
-// TODO: typedef for CharacterTestFunction
+typedef bool (*CharacterTestFunction)(char);
 
-// TODO: Implement predicate functions
+bool is_valid_char(char c) {
+    if ((c >= 32 && c <= 126 )|| c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+        return true;
+    }
+    return false;
+}
 
-int calculate_statistic(const char * text, CharacterTestFunction predicate) {
-    // TODO
+bool is_letter(char c) {
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+        return true;
+    }
+    return false;
+}
+
+bool is_digit(char c) {
+    if (c >= '0' && c <= '9') {
+        return true;
+    }
+    return false;
+}
+
+bool is_sentence(char c) {
+    if (c == '.' || c == '?' || c == '!') {
+        return true;
+    }
+    return false;
+}
+
+bool is_newline(char c) {
+    if (c == '\n') {
+        return true;
+    }
+    return false;
+}
+
+int calculate_statistic(const char * text, CharacterTestFunction test_function) {
+    int count = 0;
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (test_function(text[i])) {
+            count++;
+        }
+    }
+    return count;
+    
 }
 
 TextStatistic make_text_statistic(String text) {
-    // TODO
-    return (TextStatistic) {};
+    TextStatistic statistic = {};
+    statistic.chars = calculate_statistic(text, is_valid_char);
+    statistic.letters = calculate_statistic(text, is_letter);
+    statistic.digits = calculate_statistic(text, is_digit);
+    statistic.linebreaks = calculate_statistic(text, is_newline);
+    statistic.sentences = calculate_statistic(text, is_sentence);
+
+    return statistic;
 }
 
 void print_text_statistic(TextStatistic statistic) {
+    printf("\n\n");
     printf(".chars = %d\n", statistic.chars);
     printf(".letters = %d\n", statistic.letters);
     printf(".digits = %d\n", statistic.digits);
